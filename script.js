6e95b21e914dda435a337ca9c7f9ac4d385e2aaf
Isaac Yuri <isaacyuri22@gmail.com>
2024-10-23 07:40:49 -0300

    :recycle: refactor: blocos if/else transformados em switch case

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-var num1 = document.getElementById('n1');
-var num2 = document.getElementById('n2');
-var selecao = document.querySelector('select');
-var resultado = document.getElementById('res');
+const num1 = document.getElementById("n1");
+const num2 = document.getElementById("n2");
+const selecao = document.querySelector("select");
+const resultado = document.getElementById("res");
+
 const operacoes = {
-    somar: (n1, n2) => n1 + n2,
-    subrair: (n1, n2) => n1 - n2,
-    dividir: (n1, n2) => n1 / n2,
-    multiplicar: (n1 , n2) => n1 * n2
-} 
-function calcular(){
-    if (selecao.value == 'soma'){
-        let res = operacoes.somar(Number(num1.value), Number(num2.value))
-        resultado.innerHTML = res; 
-    } else if(selecao.value == 'subtracao'){
-        let res = operacoes.subrair(Number(num1.value), Number(num2.value))
-        resultado.innerHTML = res; 
-    } else if(selecao.value == 'divisao') {
-        let res = operacoes.dividir(Number(num1.value), Number(num2.value))
-        resultado.innerHTML = res;
-    } else if(selecao.value == 'multiplicação') {
-        let res = operacoes.multiplicar(Number(num1.value), Number(num2.value))
-        resultado.innerHTML = res;
-    } else if(selecao.value == 'escolhe'){
-    	alert('Por favor selecione uma operação');
-    }
+  somar: (n1, n2) => n1 + n2,
+  subrair: (n1, n2) => n1 - n2,
+  dividir: (n1, n2) => n1 / n2,
+  multiplicar: (n1, n2) => n1 * n2,
+};
+
+function calcular() {
+  switch (selecao.value) {
+    case "soma":
+      resultado.innerHTML = operacoes.somar(
+        Number(num1.value),
+        Number(num2.value)
+      );
+      break;
+    case "divisao":
+      resultado.innerHTML = operacoes.dividir(
+        Number(num1.value),
+        Number(num2.value)
+      );
+    case "subtrair":
+      resultado.innerHTML = operacoes.subrair(
+        Number(num1.value),
+        Number(num2.value)
+      );
+    case "multiplica":
+      resultado.innerHTML = operacoes.multiplicar(
+        Number(num1.value),
+        Number(num2.value)
+      );
+    default:
+      alert("Por favor selecione uma operação");
+      break;
+  }
 }
